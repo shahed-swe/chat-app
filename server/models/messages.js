d770bcbf741a3042a68fe92291040ddeeb490611
Md Shahed Talukder <shahedtalukder51@gmail.com>
2023-08-05 08:36:14 +0600

    database connected and models are created

---
+++ server/models/messages.js
@@ -0,0 +1,28 @@
+// Importing the Mongoose library to work with MongoDB
+const mongoose = require("mongoose");
+
+// Creating a Mongoose Schema to define the structure of the 'messages' collection
+const MessageSchema = mongoose.Schema(
+    {
+        // Defining a field 'message' with a sub-field 'text' of type String, which is required
+        message: {
+            text: { type: String, required: true },
+        },
+
+        // Defining a field 'users' as an array, which can store multiple user-related information
+        users: Array,
+
+        // Defining a field 'sender' to store the reference to a user (User model) who sent this message
+        sender: {
+            type: mongoose.Schema.Types.ObjectId, // This field will store a MongoDB ObjectId
+            ref: "User", // This references the "User" model, meaning it refers to a user document in the "users" collection
+            required: true, // The 'sender' field is required and must reference a valid user document
+        },
+    },
+    {
+        timestamps: true, // This option automatically adds 'createdAt' and 'updatedAt' fields to the documents
+    }
+);
+
+// Creating a Mongoose model named "Messages" based on the defined schema
+module.exports = mongoose.model("Messages", MessageSchema);
---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+// Importing the Mongoose library to work with MongoDB
+const mongoose = require("mongoose");
+
+// Creating a Mongoose Schema to define the structure of the 'users' collection
+const userSchema = new mongoose.Schema({
+    // Defining a field 'username' of type String, which is required and should be between 3 to 20 characters long
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true, // Ensures that each username is unique
+    },
+
+    // Defining a field 'email' of type String, which is required, and its maximum length should be 50 characters
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Ensures that each email is unique
+        max: 50,
+    },
+
+    // Defining a field 'password' of type String, which is required, and its minimum length should be 8 characters
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+    },
+
+    // Defining a field 'isAvatarImageSet' of type Boolean, which is optional. Default value is set to 'false'
+    isAvatarImageSet: {
+        type: Boolean,
+        default: false,
+    },
+
+    // Defining a field 'avatarImage' of type String, which is optional. Default value is set to an empty string
+    avatarImage: {
+        type: String,
+        default: "",
+    },
+});
+
+// Creating a Mongoose model named "Users" based on the defined schema
+module.exports = mongoose.model("Users", userSchema);
