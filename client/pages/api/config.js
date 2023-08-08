07ba884d4ab52fb6303570e45239798163c15c91
Md Shahed Talukder <35971677+shahed-swe@users.noreply.github.com>
2023-08-08 17:13:42 +0600

    Update config.js

--- client/pages/api/config.js
+++ client/pages/api/config.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5001";
+export const host = "http://20.92.123.186/:5001";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
